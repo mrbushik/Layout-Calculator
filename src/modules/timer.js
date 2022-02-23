91d7585d2f438f93f4e0474d76792e667afa724b
Nikita Bushuev <mrbushik1@gmail.com>
2022-02-23 20:25:38 +0300

    сделаное задание

--- src/modules/timer.js
+++ src/modules/timer.js
@@ -1,9 +1,15 @@
 const timer = (deadline) => {
-
+    let idInterval;
     const timerHours = document.getElementById('timer-hours');
     const timerMinutes = document.getElementById('timer-minutes');
     const timerSeconds = document.getElementById('timer-seconds');
 
+    const zeroFormat = function (x) {
+        if (x < 10) {
+            x = '0' + x;
+        }
+        return x;
+    };
     const getTimeRemaining = () => {
         let dateStop = new Date(deadline).getTime();
         let dateNow = new Date().getTime();
@@ -23,19 +29,35 @@ const timer = (deadline) => {
         timerHours.textContent = getTime.hours;
         timerMinutes.textContent = getTime.minutes;
         timerSeconds.textContent = getTime.seconds;
-        if (getTime.timeRemaining > 0) {
-            let discountTimer = setInterval(updateClock, 1000);
-        } else {
-            clearInterval(discountTimer)
+        if (getTime.timeRemaining < 0) {
             timerHours.textContent = '00';
+            timerHours.style.color = 'red';
             timerMinutes.textContent = '00';
+            timerMinutes.style.color = 'red';
             timerSeconds.textContent = '00';
+            timerSeconds.style.color = 'red';
 
+
+        } else if (getTime.timeRemaining > 0) {
+            timerHours.textContent = zeroFormat(getTime.hours);
+            timerMinutes.textContent = zeroFormat(getTime.minutes);
+            timerSeconds.textContent = zeroFormat(getTime.seconds);
         }
 
 
     };
-    updateClock();
+    const interval = () => {
+        let intervalId;
+        let getTime = getTimeRemaining();
+        updateClock();
+        if (getTime.timeRemaining > 0) {
+            intervalId = setInterval(updateClock, 1000);
+
+        } else {
+            clearInterval(intervalId);
+        }
+    };
+    interval();
 
 
 };
