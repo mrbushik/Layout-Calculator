5d55064d4798e19658fe6ba63f881cd743c7bddd
Nikita Bushuev <mrbushik1@gmail.com>
2022-02-28 22:25:58 +0300

    делегированние на кнопку меню

--- src/modules/menu.js
+++ src/modules/menu.js
@@ -3,7 +3,7 @@ const menu = () => {
     const menu = document.querySelector('menu');
     const closeBtn = menu.querySelector('.close-btn');
     const menuItems = menu.querySelectorAll('ul>li>a');
-
+    const header = document.querySelector('header')
     const handleMenu = () => {
         //убираем анимацию если разрешение меньше 768 px
         if (document.documentElement.offsetWidth > 768) {
@@ -12,8 +12,13 @@ const menu = () => {
 
 
     };
-
-    menuBtn.addEventListener('click', handleMenu);
+    // как я понял что делегирование вешать  только на одну кнопку меню это плохо
+    header.addEventListener('click', (e) => {
+        if (e.target.closest('.menu')) {
+            handleMenu();
+        }
+    });
+    // menuBtn.addEventListener('click', handleMenu);
 
     menu.addEventListener('click', (e) => {
         if (e.target.classList == 'close-btn') {
